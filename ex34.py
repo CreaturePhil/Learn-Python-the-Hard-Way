9180692cef4e90e4ff7cef49b1d3cc96ec6a9c8d
Phil <birkal@outlook.com>
2014-06-09 15:08:14 -0700

    Exercise 34: Accessing Elements of Lists

---
+++ ex34.py
@@ -0,0 +1,5 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The first animal is a %s." % animals[0]
+print "The 2nd animal is", animals[1], "and the 3rd animal is",  animals[2]
+print "The last animal is a %s." % animals[5]
