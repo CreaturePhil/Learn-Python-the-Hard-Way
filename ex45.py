8f85fb569becda71462ba3bad3cd417d70967570
Phil <birkal@outlook.com>
2014-06-14 14:22:55 -0700

    Exercise 45: You Make a Game

---
+++ ex45.py
@@ -0,0 +1,10 @@
+# Make a game
+
+"""
+Here are your requirements:
+
+1. Make a different game from the one I made.
+2. Use more than one file, and use import to use them. Make sure you know what that is.
+3. Use one class per room and give the classes names that fit their purpose (ike GoldRoom, KoiPondRoom).
+4. Your runner will need to know about these rooms, so make a class that runs them and knows about them. There's plenty of ways to do this, but consider having each room return what room is next or setting a variable of what room is next.
+"""
